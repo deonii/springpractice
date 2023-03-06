241324723b9675e69e5a7659d391e21672a94ae0
deonii <fishingman99@naver.com>
2023-03-07 01:21:17 +0900

    jpa 설정을 통한 table 생성

---
+++ src/main/java/com/example/testproject/data/entity/ProductEntity.java
@@ -0,0 +1,25 @@
+package com.example.testproject.data.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "product")
+public class ProductEntity {
+
+    @Id
+    String productId;
+    String productName;
+    Integer productPrice;
+    Integer productStock;
+
+
+}
